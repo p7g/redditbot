9c0f094819e59a435750fc7015b342af0dcf6864
Patrick Gingras <775.pg.12@gmail.com>
2019-11-28 00:52:18 -0500

    case-insensitive subreddit lookup

--- redditbot/models.py
+++ redditbot/models.py
@@ -2,6 +2,7 @@ from typing import Iterable
 
 from tortoise import Tortoise, fields
 from tortoise.models import Model
+from tortoise.query_utils import Q
 
 
 async def init(db_url: str):
@@ -10,10 +11,26 @@ async def init(db_url: str):
     await Tortoise.generate_schemas()
 
 
+def _normalize_string(s: str) -> str:
+    return s.lower()
+
+
+class CICharField(fields.CharField):
+    def to_db_value(self, value, instance):
+        return super().to_db_value(_normalize_string(value), instance)
+
+
 class Subscription(Model):
     id = fields.IntField(pk=True)
-    channel_id = fields.TextField()
-    subreddit = fields.TextField()
+    channel_id = fields.CharField(max_length=20)
+    subreddit = fields.CharField(max_length=21)  # r/21charactersandnomore
+    normalized_subreddit = CICharField(max_length=21)
+
+    @classmethod
+    async def create(cls, channel_id, subreddit):
+        return await super().create(channel_id=channel_id,
+                                    subreddit=subreddit,
+                                    normalized_subreddit=subreddit)
 
     @classmethod
     def for_channel(cls, channel_id: int):
@@ -21,7 +38,9 @@ class Subscription(Model):
 
     @classmethod
     def for_subreddit(cls, subreddit: str):
-        return cls.filter(subreddit__in=(subreddit, 'all'))
+        normalized = subreddit  # _normalize_string(subreddit)
+        return cls.filter(
+            Q(normalized_subreddit=normalized) | Q(normalized_subreddit='all'))
 
     @classmethod
     async def subreddit_subscribers(cls, subreddit: str) -> Iterable[int]:
