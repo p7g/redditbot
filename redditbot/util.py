3952c23be731cea3e7f0f2b5eb606dc121a49408
Patrick Gingras <775.pg.12@gmail.com>
2019-11-27 23:43:23 -0500

    removed dead code

--- redditbot/util.py
+++ redditbot/util.py
@@ -1,22 +1,18 @@
 import asyncio
 import functools
-import queue
-
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from multiprocessing.context import BaseContext
-from typing import Callable, Iterable, Generator, TYPE_CHECKING
+from typing import Callable, TYPE_CHECKING
 
 import discord.ext.commands
-import janus
 
 if TYPE_CHECKING:
     import redditbot.reddit
 
 pool = ThreadPoolExecutor()
 
-__all__ = ('Context', 'generator_to_coroutine', 'sync_to_async',
-           'queue_to_async_gen')
+__all__ = ('Context', 'sync_to_async', 'queue_to_async_gen')
 
 
 @dataclass
@@ -29,32 +25,10 @@ class Context:
     mp_context: BaseContext
 
 
-def _yield_to_queue(q: queue.Queue, gen: Iterable):
-    for val in gen:
-        q.put(val)
-    q.join()
-
-
 async def queue_to_async_gen(q: asyncio.Queue):
     while True:
         yield await q.get()
         q.task_done()
-
-
-def queue_to_gen(q: queue.Queue):
-    while True:
-        yield q.get()
-        q.task_done()
-
-
-def generator_to_coroutine(gen: Callable[..., Generator]):
-    @functools.wraps(gen)
-    def wrapper(*args, **kwargs):
-        q = janus.Queue()
-        pool.submit(_yield_to_queue, q.sync_q, gen(*args, **kwargs))
-        return queue_to_async_gen(q.async_q)
-
-    return wrapper
 
 
 def sync_to_async(fn: Callable):
